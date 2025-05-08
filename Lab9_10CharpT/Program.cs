85f6bd61af18792007f21a0af85784294d52249e
Stas <stanislavantimichuk@gmail.com>
2025-05-08 10:16:01 +0300

    The end.

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
+﻿//  За бажанням студента для задач можна створювати консольний проект або WinForm
 // Бажано для задач лаб. робіт створити окремі класи
 // Виконання  виконати в стилі багатозаданості :
 //   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
 // При бажанні можна створити багатозадачний режим виконання задач.
 
+using Lab9_10CharpT;
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+
+Trader bull1 = new Trader("Бик Джон", TraderType.Bull, 100.0m);
+Trader bull2 = new Trader("Бик Боб", TraderType.Bull, 100.0m);
+Trader bear1 = new Trader("Ведмідь Іван", TraderType.Bear, 100.0m);
+Trader bear2 = new Trader("Ведмідь Віктор", TraderType.Bear, 100.0m);
+
+bull1.PriceChanged += bull2.OnPriceChanged;
+bull1.PriceChanged += bear1.OnPriceChanged;
+bull1.PriceChanged += bear2.OnPriceChanged;
+
+bull2.PriceChanged += bull1.OnPriceChanged;
+bull2.PriceChanged += bear1.OnPriceChanged;
+bull2.PriceChanged += bear2.OnPriceChanged;
+
+bear1.PriceChanged += bull1.OnPriceChanged;
+bear1.PriceChanged += bull2.OnPriceChanged;
+bear1.PriceChanged += bear2.OnPriceChanged;
+
+bear2.PriceChanged += bull1.OnPriceChanged;
+bear2.PriceChanged += bull2.OnPriceChanged;
+bear2.PriceChanged += bear1.OnPriceChanged;
+
+Console.WriteLine("Симуляція зміни ціни акції:");
+
+Console.WriteLine("First.");
+bull1.ChangePrice("AnimalCoin", 110.0m);
+Console.WriteLine();
+
+Console.WriteLine("Second.");
+bear1.ChangePrice("AnimalCoin", 90.0m);
+Console.WriteLine();
---
+++ Lab9_10CharpT/Trader.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_10CharpT
+{
+    public enum TraderType
+    {
+        Bull,
+        Bear 
+    }
+
+    public class Trader
+    {
+        public string Name { get; }
+        public TraderType Type { get; }
+        private decimal currentPrice;
+
+        public event PriceChangeEventHandler PriceChanged;
+
+        public Trader(string name, TraderType type, decimal initialPrice)
+        {
+            Name = name;
+            Type = type;
+            currentPrice = initialPrice;
+        }
+
+        public void ChangePrice(string stockSymbol, decimal newPrice)
+        {
+            if (newPrice < 0)
+            {
+                Console.WriteLine($"{Name}: Ціна не може бути від'ємною!");
+                return;
+            }
+
+            decimal oldPrice = currentPrice;
+            currentPrice = newPrice;
+
+            PriceChanged?.Invoke(this, new PriceChangeEventArgs(stockSymbol, oldPrice, newPrice));
+        }
+
+        public void OnPriceChanged(object sender, PriceChangeEventArgs args)
+        {
+            if (sender == this)
+                return;
+
+            Console.WriteLine($"{Name} ({Type}) реагує на зміну ціни акції {args.StockSymbol}: {args.OldPrice} -> {args.NewPrice}");
+
+            if (Type == TraderType.Bull)
+            {
+                if (args.NewPrice > args.OldPrice)
+                    Console.WriteLine($"{Name}: Ціна зросла! Бик у виграші!\n");
+                else if (args.NewPrice < args.OldPrice)
+                    Console.WriteLine($"{Name}: Ціна впала... Бик у програші.\n");
+            }
+            else // Bear
+            {
+                if (args.NewPrice < args.OldPrice)
+                    Console.WriteLine($"{Name}: Ціна впала! Ведмідь у виграші!\n");
+                else if (args.NewPrice > args.OldPrice)
+                    Console.WriteLine($"{Name}: Ціна зросла... Ведмідь у програші.\n");
+            }
+        }
+    }
+
+    public class PriceChangeEventArgs : EventArgs
+    {
+        public string StockSymbol { get; }
+        public decimal OldPrice { get; }
+        public decimal NewPrice { get; }
+
+        public PriceChangeEventArgs(string stockSymbol, decimal oldPrice, decimal newPrice)
+        {
+            StockSymbol = stockSymbol;
+            OldPrice = oldPrice;
+            NewPrice = newPrice;
+        }
+    }
+    public delegate void PriceChangeEventHandler(object sender, PriceChangeEventArgs args);
+
+
+}
